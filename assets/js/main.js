3d830681ced7671a80ab79197cd58a8e0aa3018d
Sirajudeen <ksiraj2002@gmail.com>
2022-06-20 21:51:07 +0530

    scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,6 +49,7 @@
 	    animateIn: 'fadeIn',
 	    nav:false,
 	    autoplayHoverPause: false,
+		disable:scroll,
 	    items: 1,
 	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
 	    responsive:{
